50aaba6fd83fd65c9e36e2f693d305eed50c4ac4
yeonkr <1714882@sookmyung.ac.kr>
2022-09-15 15:29:55 +0900

    feat: 프로젝트 초기 설정

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,7 @@
-import React from "react";
+import React from 'react';
 
-const App  = () => {
-    return(
-        <div>
-            제발좀되라
-        </div>
-    )
-}
+const App = () => {
+  return <div>제발좀되라</div>;
+};
 
 export default App;
