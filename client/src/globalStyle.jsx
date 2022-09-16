d1ad1a421757c6b6a23bd6fa2dd6b95e130c3ff7
JoEunsoo <dcpg95@gmail.com>
2022-09-16 09:22:56 +0900

    feat : 공통으로 사용할 style 코드

---
+++ client/src/globalStyle.jsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    :root {
+        // Color
+        --lightgray : #d9d9d9;
+        --gray : #aaaaaa;
+        --red : red;
+        
+        // font-size
+        --xsmall : 9px;
+        --small : 13px;
+        --medium : 20px;
+        --large : 28px;
+        --xlarge : 36px;
+        //
+`;
+
+export default GlobalStyle;
