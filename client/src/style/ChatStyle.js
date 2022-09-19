705f3fbb8ba7638ebec0b8f9fed268e426f422a6
JoEunsoo <dcpg95@gmail.com>
2022-09-20 00:03:50 +0900

    feat : 채팅 기능구현

---
+++ client/src/style/ChatStyle.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const ChatContainer = styled.div`
+    border : 1px solid black;
+    width : 500px;
+    height: 400px;
+    margin: 30px;
+`
+
+export {ChatContainer};
