b27eba3f82b360c62af59efad77f2431fec10b07
JoEunsoo <dcpg95@gmail.com>
2022-09-19 16:08:22 +0900

    feat : 채팅 기능 구현

---
+++ client/src/Components/Chat.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Chat = () => {
+    return(
+        <>
+            <ChatUI />
+        </>
+    )
+}
+
+const ChatUI = () => {
+    return(
+        <div>
+            sdasdasd
+        </div>
+    )
+}
+
+export default Chat;
