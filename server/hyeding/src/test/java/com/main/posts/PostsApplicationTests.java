7cff27bb9c724771ce38aa9d96e9d562b690d7e1
KimJiHong3 <94733559+KimJiHong3@users.noreply.github.com>
2022-09-20 10:14:33 +0900

    feat: posts 테스트 api구현

---
+++ server/hyeding/src/test/java/com/main/posts/PostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
