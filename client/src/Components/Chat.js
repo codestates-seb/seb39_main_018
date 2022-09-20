21afbbaceec575d2469a88dc3eca06d00aac643f
JoEunsoo <dcpg95@gmail.com>
2022-09-20 15:45:50 +0900

    feat : 채팅기능 구현

--- client/src/Components/Chat.js
+++ client/src/Components/Chat.js
@@ -1,80 +1,96 @@
-import React, { useEffect, useState } from "react";
-import { ChatContainer } from "../style/ChatStyle";
-import io from "socket.io-client";
+import React, { useEffect, useState } from 'react';
+import { ChatContainer, Scrollbar, ChatContent } from '../style/ChatStyle';
+import io from 'socket.io-client';
 
-const endpoint = 'https://jjjserver.herokuapp.com/'; // heroku로 socket 배포한 서버
-let socket = io(endpoint , {
-    cors: {
-        origin : '*'
-    }
+const endpoint = 'http://localhost:8080'; // heroku로 socket 배포한 서버
+let socket = io(endpoint, {
+  cors: {
+    origin: '*',
+  },
 });
 
-
 const Chat = () => {
-    return(
-        <>
-            <ChatUI />
-        </>
-    )
-}
+  return (
+    <>
+      <ChatUI />
+    </>
+  );
+};
 
 const ChatUI = () => {
+  const [chatMsg, setChatMsg] = useState([]);
+  const [chatText, setChatText] = useState({});
+  const [ChatID, setChatID] = useState({});
+  console.log(ChatID);
 
-   const [ chatMsg, setChatMsg ] = useState([]);
-   const [msg , setMsg] = useState({user : ''});
+  const send = () => {
+    socket.emit('inputMsg', chatText);
+    setChatText({ id: '', message: '' });
+  };
 
-    // let msg = {id : 'babo' , user: ''}
+  useEffect(() => {
+    socket.on('inputId', (res) => {
+      setChatMsg([...chatMsg, res]);
+    });
+    socket.on('inputMsg', (res) => {
+        setChatMsg([...chatMsg, res]);
+      });
+  }, [chatMsg]);
 
-    const send = () => {
-        socket.emit("message", msg );
-        setMsg({user : ''});
-      };
-    
-      useEffect(() => {
-        socket.on("message", (message) => {
-            setChatMsg([...chatMsg, message]);
-        });
-      }, [chatMsg]);
-      console.log(chatMsg)
 
-    const messages = (e) => {
-        let user = e.target.value;
-        setMsg({user});
-        if(e.key === 'Enter'){
-            socket.emit("message", msg );
-            setMsg({user : ''});
-        }
-       
+  const inputHandle = (key) => (e) => {
+    if (key === 'id') setChatID({[key] : e.target.value });
+    setChatText({[key] : e.target.value });
+  };
+
+  const sendEnter = (e) => {
+    console.log(chatText);
+    if (e.key === 'Enter') {
+      socket.emit('inputData', chatText);
+      setChatText({ id: '', message: '' });
     }
+  };
 
-    return(
-        <>
-        <ChatContainer>
-            <div>
-       
-            </div>
-            
-               {chatMsg.map( (li,i) => {
-                return(
-                    <div key={i} className="chat_body">
-                        {li}
-                    </div>
-                    
-                )
-               })}
-           
-        </ChatContainer>
-        <ChatContainer>
-            <label>
-                채팅입력
-            </label>
-            <input id="isinput" onChange={messages} onKeyUp={messages} value={msg.user}/>
-            <button onClick={send}>
-                전송
-            </button>
-        </ChatContainer>
-        </>
-    )
-}
+  return (
+    <>
+      <ChatContainer>
+        <div></div>
+        {chatMsg.map((li, i) => {
+          return (
+            <>
+              <p>{chatMsg[0].id}</p>
+              <div key={i} className="chat_body">
+                {li.message}
+              </div>
+            </>
+          );
+        })}
+        <Scrollbar></Scrollbar>
+      </ChatContainer>
+      <ChatContainer>
+        <ChatContent>
+          <div className="iss">
+            <label>아이디입력</label>
+            <input
+              onChange={inputHandle('id')}
+              onKeyUp={sendEnter}
+              value={chatText.id}
+            />
+            <button onClick={send}>전송</button>
+          </div>
+          <div className="iss">
+            <label>채팅입력</label>
+            <input
+              onChange={inputHandle('message')}
+              onKeyUp={sendEnter}
+              value={chatText.message}
+            />
+            <button onClick={send}>전송</button>
+          </div>
+        </ChatContent>
+      </ChatContainer>
+    </>
+  );
+};
 
 export default Chat;
