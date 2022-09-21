67b900f2f6848f786d0a7185e643927ce3eb0973
JoEunsoo <dcpg95@gmail.com>
2022-09-21 11:56:38 +0900

    refactor : 컴포넌트 이름 수정

--- client/src/common/ui/Tag.js
+++ client/src/common/ui/Tag.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TagContainer } from '../../style/TagStyle';
+import MyTag from '../../style/TagStyle';
 import { MdCancel } from 'react-icons/md';
 
 const Tag = () => {
@@ -8,16 +8,22 @@ const Tag = () => {
 
 const TagUI = () => {
   return (
-    <TagContainer>
-      <p className="tag_text">센프란시스코</p>
-      <p className="tag_cancel">
-        <MdCancel />
-      </p>
-      <div className="circle1"></div>
-      <div className="circle2"></div>
-      <div className="circle3"></div>
-      <div className="circle4"></div>
-    </TagContainer>
+    <>
+      <MyTag.TagContainer>
+        <p className="tag_text">센프란시스코</p>
+        <p className="tag_cancel">
+          <MdCancel />
+        </p>
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+        <div className="circle3"></div>
+        <div className="circle4"></div>
+      </MyTag.TagContainer>
+      <MyTag.Tag1>
+
+      </MyTag.Tag1>
+      <MyTag.Tag2></MyTag.Tag2>
+    </>
   );
 };
 
