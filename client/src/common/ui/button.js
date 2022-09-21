4119f015fc7c3c9e6dca92b1c61177e04558194a
JoEunsoo <dcpg95@gmail.com>
2022-09-21 16:30:21 +0900

    refactor : 버튼 컴포넌트코드,css 수정

--- client/src/common/ui/button.js
+++ client/src/common/ui/button.js
@@ -4,62 +4,116 @@ import { HiOutlineTicket } from 'react-icons/hi';
 
 const BtnComponent = () => {
   return (
-    <>
-      <Buttons />
-    </>
+    <Btn.MainContaier>
+      <Btn.Test>
+        <TestBtn />
+      </Btn.Test>
+      <Btn.Post>
+        <Btn.FrameTest>
+          <SellBtn />
+        </Btn.FrameTest>
+        <Btn.FrameTest>
+          <ReadBtn />
+        </Btn.FrameTest>
+        <Btn.FrameTest>
+          <EditBtn />
+        </Btn.FrameTest>
+        <Btn.FrameTest>
+          <DeleteBtn />
+        </Btn.FrameTest>
+        <Btn.FrameTest>
+          <MessageBtn />
+        </Btn.FrameTest>
+      </Btn.Post>
+      <Btn.Basic>
+        <Btn.FrameTest>
+          <LikeBtn />
+        </Btn.FrameTest>
+        <Btn.FrameTest>
+          <CancelBtn />
+        </Btn.FrameTest>
+        <Btn.FrameTest>
+          <OkBtn />
+        </Btn.FrameTest>
+        <Btn.FrameTest>
+          <CreateBtn />
+        </Btn.FrameTest>
+        <Btn.FrameTest>
+          <AuthBtn />
+        </Btn.FrameTest>
+        <Btn.FrameTest>
+          <DropOutBtn />
+        </Btn.FrameTest>
+      </Btn.Basic>
+    </Btn.MainContaier>
+  );
+};
+export default BtnComponent;
+
+const SellBtn = () => {
+  return (
+    <Btn.AllSets>
+      <Btn.Sell>
+        <div className="icon">
+          <HiOutlineTicket />
+        </div>
+        <div className="text">판매하기</div>
+      </Btn.Sell>
+    </Btn.AllSets>
   );
 };
 
-const Buttons = () => {
+const CancelBtn = () => {
+  return <Btn.Cancel>취소</Btn.Cancel>;
+};
+const CreateBtn = () => {
+  return <Btn.Posting>등록하기</Btn.Posting>;
+};
+
+const AuthBtn = () => {
+  return <Btn.Auth>인증하기</Btn.Auth>;
+};
+const DropOutBtn = () => {
+  return <Btn.DropOut>탈퇴신청</Btn.DropOut>;
+};
+const OkBtn = () => {
+  return <Btn.Ok>확인</Btn.Ok>;
+};
+const TestBtn = () => {
   return (
     <>
-      <Btn.MainContaier>
-        <Btn.AllSets>
-          <Btn.Sell>
-            <div className="icon">
-              <HiOutlineTicket />
-            </div>
-            <div className="text">판매하기</div>
-          </Btn.Sell>
-        </Btn.AllSets>
-        <Btn.AllSets>
-          <Btn.Posting>등록하기</Btn.Posting>
-        </Btn.AllSets>
-        <Btn.AllSets>
-          <Btn.Auth>인증하기</Btn.Auth>
-        </Btn.AllSets>
-        <Btn.AllSets>
-          <Btn.DropOut>탈퇴신청</Btn.DropOut>
-        </Btn.AllSets>
-        <Btn.AllSets>
-          <Btn.Ok>확인</Btn.Ok>
-        </Btn.AllSets>
-        <Btn.AllSets>
-          <Btn.Cancel>취소</Btn.Cancel>
-        </Btn.AllSets>
-
-        <Btn.XXBtn>눌렀을때</Btn.XXBtn>
-        <Btn.XXBtn2>누르기전</Btn.XXBtn2>
-      </Btn.MainContaier>
-      <Btn.MainContaier>
-        <Btn.AllSets>
-          <Btn.Edit>게시글수정</Btn.Edit>
-        </Btn.AllSets>
-        <Btn.AllSets>
-          <Btn.Delete>게시글삭제 </Btn.Delete>
-        </Btn.AllSets>
-        <Btn.AllSets>
-          <Btn.Different>다른상품보기 </Btn.Different>
-        </Btn.AllSets>
-        <Btn.AllSets>
-          <Btn.Like>찜하기</Btn.Like>
-        </Btn.AllSets>
-        <Btn.AllSets>
-          <Btn.Message>메세지보내기</Btn.Message>
-        </Btn.AllSets>
-      </Btn.MainContaier>
+      <Btn.XXBtn>눌렀을때</Btn.XXBtn>
+      <Btn.XXBtn2>누르기전</Btn.XXBtn2>
     </>
   );
 };
+const EditBtn = () => {
+  return <Btn.Edit>게시글수정</Btn.Edit>;
+};
 
-export default BtnComponent;
+const DeleteBtn = () => {
+  return <Btn.Delete>게시글삭제 </Btn.Delete>;
+};
+const ReadBtn = () => {
+  return <Btn.Different>다른상품보기 </Btn.Different>;
+};
+const LikeBtn = () => {
+  return <Btn.Like>찜하기</Btn.Like>;
+};
+const MessageBtn = () => {
+  return <Btn.Message>메세지보내기</Btn.Message>;
+};
+
+export {
+  SellBtn,
+  ReadBtn,
+  EditBtn,
+  DeleteBtn,
+  MessageBtn,
+  LikeBtn,
+  CancelBtn,
+  OkBtn,
+  CreateBtn,
+  AuthBtn,
+  DropOutBtn,
+};
