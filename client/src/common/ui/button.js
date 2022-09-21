f9948f50e626c5377ce772cbaee1db3c8f39a991
JoEunsoo <dcpg95@gmail.com>
2022-09-21 11:52:49 +0900

    refactor : 버튼 컴포넌트 코드수정

--- client/src/common/ui/button.js
+++ client/src/common/ui/button.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import {
-  MainContaier,
-  SellButton,
-  SellText,
-  SellIcon,
-  BasicBtn,
-  XXBtn,
-  XXBtn2,
-  PostPageBtn,
-} from '../../style/ButtonStyle';
+import Btn from '../../style/ButtonStyle';
 import { HiOutlineTicket } from 'react-icons/hi';
 
 const BtnComponent = () => {
@@ -22,29 +13,51 @@ const BtnComponent = () => {
 const Buttons = () => {
   return (
     <>
-      <MainContaier>
-        <SellButton>
-          <SellIcon>
-            <HiOutlineTicket className="icon" />
-          </SellIcon>
-          <SellText>판매하기</SellText>
-        </SellButton>
-        <BasicBtn>등록하기</BasicBtn>
-        <BasicBtn>인증하기</BasicBtn>
-        <BasicBtn>탈퇴신청</BasicBtn>
-        <BasicBtn>확인</BasicBtn>
-        <BasicBtn>취소</BasicBtn>
-        <XXBtn>눌렀을때</XXBtn>
-        <XXBtn2>누르기전</XXBtn2>
-      </MainContaier>
-      <MainContaier>
-        <PostPageBtn>게시글수정</PostPageBtn>
-        <PostPageBtn>게시글삭제</PostPageBtn>
-        <PostPageBtn>게시글페이지</PostPageBtn>
-        <PostPageBtn>다른상품보기</PostPageBtn>
-        <PostPageBtn>찜하기</PostPageBtn>
-        <PostPageBtn>메세지보내기</PostPageBtn>
-      </MainContaier>
+      <Btn.MainContaier>
+        <Btn.AllSets>
+          <Btn.Sell>
+            <div className="icon">
+              <HiOutlineTicket />
+            </div>
+            <div className="text">판매하기</div>
+          </Btn.Sell>
+        </Btn.AllSets>
+        <Btn.AllSets>
+          <Btn.Posting>등록하기</Btn.Posting>
+        </Btn.AllSets>
+        <Btn.AllSets>
+          <Btn.Auth>인증하기</Btn.Auth>
+        </Btn.AllSets>
+        <Btn.AllSets>
+          <Btn.DropOut>탈퇴신청</Btn.DropOut>
+        </Btn.AllSets>
+        <Btn.AllSets>
+          <Btn.Ok>확인</Btn.Ok>
+        </Btn.AllSets>
+        <Btn.AllSets>
+          <Btn.Cancel>취소</Btn.Cancel>
+        </Btn.AllSets>
+
+        <Btn.XXBtn>눌렀을때</Btn.XXBtn>
+        <Btn.XXBtn2>누르기전</Btn.XXBtn2>
+      </Btn.MainContaier>
+      <Btn.MainContaier>
+        <Btn.AllSets>
+          <Btn.Edit>게시글수정</Btn.Edit>
+        </Btn.AllSets>
+        <Btn.AllSets>
+          <Btn.Delete>게시글삭제 </Btn.Delete>
+        </Btn.AllSets>
+        <Btn.AllSets>
+          <Btn.Different>다른상품보기 </Btn.Different>
+        </Btn.AllSets>
+        <Btn.AllSets>
+          <Btn.Like>찜하기</Btn.Like>
+        </Btn.AllSets>
+        <Btn.AllSets>
+          <Btn.Message>메세지보내기</Btn.Message>
+        </Btn.AllSets>
+      </Btn.MainContaier>
     </>
   );
 };
