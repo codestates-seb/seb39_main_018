763b9a2ebed8accad6080932f02c286cffe10899
yeonkr <1714882@sookmyung.ac.kr>
2022-09-21 14:36:47 +0900

    feat : Kakao Login 구현

--- client/src/Components/SocialLogin/KakaoLogin.js
+++ client/src/Components/SocialLogin/KakaoLogin.js
@@ -4,7 +4,7 @@ import { KakaoLoginButton } from './KakaoLoginStyle';
 
 const KAKA0_HOST = 'https://kauth.kakao.com/oauth/authorize';
 const ClientID = process.env.KAKAO_CLIENT_ID;
-const REDIRECT_URL = 'http://localhost:3003/oauth/callback/';
+const REDIRECT_URL = 'http://localhost:3003/oauth/callback/kakao';
 
 const requestUri = `${KAKA0_HOST}?client_id=${ClientID}&redirect_uri=${REDIRECT_URL}&response_type=code`;
 
