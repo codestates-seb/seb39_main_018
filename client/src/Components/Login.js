25c7470fe5a5ecfa2fd20128f374d786587ccfec
JoEunsoo <dcpg95@gmail.com>
2022-09-26 18:03:41 +0900

    feat: 비밀번호 찾기 스타일 적용

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { useId } from 'react';
 import { useState } from 'react';
-import { get } from '../util/axios';
 import { useRef } from 'react';
 import KakaoLogin from './SocialLogin/KakaoLogin';
 import Logins from '../style/LoginStyle';
 import NaverLogin from './SocialLogin/NaverLogin';
+import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
   const idInput = useRef();
   const passwordInput = useRef();
-  const [userid, setUserId] = useState('');
-  const [userPassword, setUserPassword] = useState('');
+  const [userid, setUserId] = useState({ value: '', text: '', hidden: true });
+  const [userPassword, setUserPassword] = useState({ value: '', text: '', hidden: true });
 
-  const userlogin = async () => {
-    if (userid !== '' && userPassword !== '') {
-      let parameter = {};
-      parameter = {
-        userid: userid.value,
-        userPassword: userPassword.value,
-      };
-      const loginClient = await get('url', parameter);
-      return loginClient;
-    }
-    if (userid === '') {
+  const userlogin = () => {
+    console.log(userid.value);
+    console.log(userPassword.value);
+
+    if (userid.value === '') {
       idInput.current.focus();
-    } else if (userPassword === '') {
+      return;
+    } else if (userPassword.value === '') {
       passwordInput.current.focus();
+      return;
+    }
+
+    if (userid.value !== '' && userPassword.value !== '') {
+      axios
+        .post('http://ec2-3-34-181-86.ap-northeast-2.compute.amazonaws.com:8080/board', {
+          headers: { 'Content-Type': 'application/json' },
+          id: id,
+          password: password,
+        })
+        .then((res) => {
+          resolve(res);
+          window.alert('로그인 성공!');
+          navigate('/');
+
+          if (checkedBox) {
+            localStorage.setItem('login', token); // 서버 토큰값
+          } else {
+            sessionStorage.setItem('login', token);
+          }
+        })
+        .catch((err) => {
+          window.alert('로그인 실패!');
+          console.log(err);
+        });
+    } else {
+      console.log('로그인 실패');
     }
   };
 
@@ -45,16 +67,19 @@ const Login = () => {
     setUserPassword({ value: value, text: '', hidden: true });
   };
 
+  const SignupClick = () => {
+    props.signupClickInLogin();
+  };
+
+  const FindClick = () => {
+    props.findClickInLogin();
+  };
+
   // 자동로그인 체크박스
-  const [checkedBox, setCheckedBox] = useState(new Set());
+  const [checkedBox, setCheckedBox] = useState(false);
   // 체크 O
   const handleCheckdBox = (id, isChecked) => {
     if (isChecked) {
-      checkedBox.add(id);
-      setCheckedBox(checkedBox);
-      // 체크 X -> delete
-    } else if (!isChecked && checkedBox.has(id)) {
-      checkedBox.delete(id);
       setCheckedBox(checkedBox);
     }
   };
@@ -68,8 +93,9 @@ const Login = () => {
         </Logins.TopHeader>
 
         <Logins.IdBox>
-          <Logins.NameBox>아이디</Logins.NameBox>
+          <Logins.NameBox color={!userid.hidden ? 'red' : 'black'}>아이디</Logins.NameBox>
           <Logins.InputBox
+            color={!userid.hidden ? 'red' : 'black'}
             ref={idInput}
             id="id"
             type="text"
@@ -81,8 +107,9 @@ const Login = () => {
         </Logins.IdBox>
 
         <Logins.PasswordBox>
-          <Logins.NameBox>비밀번호</Logins.NameBox>
+          <Logins.NameBox color={!userPassword.hidden ? 'red' : 'black'}>비밀번호</Logins.NameBox>
           <Logins.InputBox
+            color={!userPassword.hidden ? 'red' : 'black'}
             ref={passwordInput}
             id="password"
             type="password"
@@ -97,7 +124,7 @@ const Login = () => {
         <Logins.LinkBox>
           <input type="checkbox" onChange={(e) => handleCheckdBox(e)} />
           <Logins.AutoLogin>로그인 유지하기</Logins.AutoLogin>
-          <Logins.IdOrPwFind>ID/PW 찾기</Logins.IdOrPwFind>
+          <Logins.IdOrPwFind onClick={() => FindClick()}>PW 찾기</Logins.IdOrPwFind>
         </Logins.LinkBox>
 
         <Logins.Button
@@ -112,7 +139,7 @@ const Login = () => {
 
         <Logins.GoSignUp>
           <Logins.AreYouAMember>회원이 아니신가요?</Logins.AreYouAMember>
-          <Logins.RealJoin>회원가입</Logins.RealJoin>
+          <Logins.RealJoin onClick={() => SignupClick()}>회원가입</Logins.RealJoin>
         </Logins.GoSignUp>
       </Logins.Box>
     </Logins.Container>
