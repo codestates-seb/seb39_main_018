583f9449bcff84d1a6750d0f9a95916c82daae5b
JoEunsoo <dcpg95@gmail.com>
2022-09-27 20:36:37 +0900

    feat : 상품 날짜 알고리즘 구현

--- client/src/util/detailLogic.js
+++ client/src/util/detailLogic.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-const saves = () => {
+const saves = (id) => {
   const [data, setdata] = useState('');
-  const id = '';
-  const url = `http://ec2-3-34-181-86.ap-northeast-2.compute.amazonaws.com:8080/board`;
+  const url = `http://ec2-3-34-181-86.ap-northeast-2.compute.amazonaws.com:8080/board/${id}`;
   useEffect(() => {
     axios(url, {
       method: 'get',
@@ -31,5 +30,27 @@ const itemGet = () => {
   return data;
 };
 
-export { saves , itemGet}
+const dateCompute = (date) => {
+  const nowDate = new Date();
+  const createdDate = new Date(date);
+  const dateCount = nowDate.getTime() - createdDate.getTime();
+  let min = Math.floor(dateCount / (1000 * 60));
+  let hor = Math.floor(dateCount / (1000 * 60 * 60));
+  let day = Math.floor(dateCount / (1000 * 60 * 60 * 24));
+  let mon = Math.floor(dateCount / (1000 * 60 * 60 * 24 * 30));
+  let year = Math.floor(dateCount / (1000 * 60 * 60 * 24 * 30 * 12));
+
+  return year
+    ? `${year} 년전`
+    : mon
+    ? `${mon} 개월전`
+    : day
+    ? `${day} 일전`
+    : hor
+    ? `${hor} 시간전`
+    : min
+    ? `${min} 분전`
+    : '방금전';
+};
+export { saves , itemGet, dateCompute}
 
