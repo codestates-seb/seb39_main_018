dd296d7c271a331973c118aa6d75cebbdd3cd3b7
JoEunsoo <dcpg95@gmail.com>
2022-09-27 09:32:08 +0900

    feat : 상품 리스트 함수 구현

--- client/src/Components/ItemList/Item.js
+++ client/src/Components/ItemList/Item.js
@@ -9,8 +9,20 @@ import {
   ItemPrice,
   SellInfo,
 } from './ItemStyle';
+import { itemGet } from '../../util/detailLogic';
+import { useParams } from 'react-router-dom';
 
 const ItemList = () => {
+  // const Itemdata = itemGet();
+  // Itemdata.map((itemListdata, key) => {
+  //   itemListdata.id;
+  //   itemListdata.img;
+  //   itemListdata.title;
+  //   itemListdata.price;
+  //   itemListdata.region;
+  //   itemListdata.createDate;
+  // });
+
   return (
     <ItemContainer>
       <ItemsCount />
@@ -34,17 +46,17 @@ export default ItemList;
 
 const ItemComponent = () => {
   return (
-    <ItemBox>
-        <ItemImg></ItemImg>
-        <ItemTitle>용산에서 부산행 KTX 표 팝니다.</ItemTitle>
-        <ItemPrice>
-            <p className='price_data'>25,000</p>
-            <p>원</p>
-            </ItemPrice>
-        <SellInfo>
-            <p className='sell_area'>서울시 강남구 논현동</p>
-            <p className='posted_date'>1일전</p>
-        </SellInfo>
+    <ItemBox onClick={() => useParams()}>
+      <ItemImg></ItemImg>
+      <ItemTitle>용산에서 부산행 KTX 표 팝니다.</ItemTitle>
+      <ItemPrice>
+        <p className="price_data">25,000</p>
+        <p>원</p>
+      </ItemPrice>
+      <SellInfo>
+        <p className="sell_area">서울시 강남구 논현동</p>
+        <p className="posted_date">1일전</p>
+      </SellInfo>
     </ItemBox>
   );
 };
