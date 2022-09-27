9ed1936b76107504599265a537e67aaed14ffec0
JoEunsoo <dcpg95@gmail.com>
2022-09-28 00:32:35 +0900

    feat : 메인페이지에 사용할 tag 구현

--- client/src/Components/Common/Tag/Tag.js
+++ client/src/Components/Common/Tag/Tag.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { CancelIcon } from '../Icons/Icons';
 import Tag from './TagStyle';
 
-
 // 함수용 컴포넌트
 const TagComponent = () => {
   return;
@@ -21,18 +20,14 @@ const TagUI = () => {
         <Tag.Circle3 />
         <Tag.Circle4 />
       </Tag.Container>
+    </>
+  );
+};
+const BasicTag = () => {
+  return (
+    <>
       <Tag.Container>
-        <p className="tag_text">비행기티켓</p>
-        <p className="tag_cancel">
-          <CancelIcon />
-        </p>
-        <Tag.Circle1 />
-        <Tag.Circle2 />
-        <Tag.Circle3 />
-        <Tag.Circle4 />
-      </Tag.Container>
-      <Tag.Container>
-        <p className="tag_text">솔트</p>
+        <p className="tag_text">센프란시스코</p>
         <p className="tag_cancel">
           <CancelIcon />
         </p>
@@ -45,4 +40,4 @@ const TagUI = () => {
   );
 };
 
-export default TagUI;
+export { TagUI, Tag };
