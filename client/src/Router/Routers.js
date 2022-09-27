508fd9fe4ac91ef3a252df5b8233262214544101
JoEunsoo <dcpg95@gmail.com>
2022-09-27 09:33:26 +0900

    refactor : init 삭제

--- client/src/Router/Routers.js
+++ client/src/Router/Routers.js
@@ -13,12 +13,7 @@ import Header from '../Components/Common/Header/Header';
 import Footer from '../Components/Common/Footer/Footer';
 import React, { useEffect } from 'react';
 
-
 const Routers = () => {
-  useEffect(() => {
-    window.Kakao.init('7f72f6bd7dc714fa93bd9794498a7a2b');
-  }, []);
-
   return (
     <>
       <Pages />
@@ -34,7 +29,7 @@ const Pages = () => {
       <Route path="/" element={<MainPage />} />
       <Route path="/chat" element={<ChatPage />} />
       <Route path="/mypage" element={<MyPage />} />
-      <Route path="/detail" element={<DetailPage />} />
+      <Route path="/detail/12" element={<DetailPage />} />
     </Routes>
   );
 };
