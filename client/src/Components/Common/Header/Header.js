6ff7ed57544d070a515b3fee5c96b2273b7e5db7
JoEunsoo <dcpg95@gmail.com>
2022-09-29 23:06:55 +0900

    feat : 헤더 컴포넌트 구현

--- client/src/Components/Common/Header/Header.js
+++ client/src/Components/Common/Header/Header.js
@@ -1,55 +1,112 @@
 import React from 'react';
+import { InputUI } from '../Input/Input';
+import manIcon from '../../Imgs/man.png';
+import message from '../../Imgs/paper-airplane.png';
+import message2 from '../../Imgs/menu.png';
+import burger from '../../Imgs/menu.png';
 import {
   HeaderContainer,
-  Header_MainSection,
-  Header_SubSection,
+  NavbarBottom,
+  NavbarSection,
+  NavbarTop,
   LogoSection,
   SearchSection,
   SearchMain,
   ButtonSection,
   CategorySection,
+  CategoryMain,
 } from './HeaderStyle';
+import { useNavigate } from 'react-router-dom';
 const Header = () => {
   return (
     <HeaderContainer>
-      <Header_MainSection>
-        <HeaderLogo />
-        <HeaderSearch />
-        <HeaderButton />
-      </Header_MainSection>
-      <Header_SubSection>
-        <HeaderCategory />
-      </Header_SubSection>
+      <NavbarSection>
+        <NavbarTop>
+          <NavLogo />
+          <NavSearch />
+          <NavButton />
+        </NavbarTop>
+        <NavbarBottom>
+          <NavCategory />
+        </NavbarBottom>
+      </NavbarSection>
     </HeaderContainer>
   );
 };
 
-const HeaderLogo = () => {
+const NavLogo = () => {
   return (
     <LogoSection>
       <p>SORT</p>
     </LogoSection>
   );
 };
-const HeaderSearch = () => {
+const NavSearch = () => {
   return (
     <SearchSection>
-      <SearchMain></SearchMain>
+      <SearchMain>
+        <InputUI
+          placeholder="원하는 상품을 입력해주세요"
+          radius="20px"
+          border="2px solid gray"
+          padding="15px"
+          size="15px"
+          color="gray"
+        />
+      </SearchMain>
     </SearchSection>
   );
 };
 
-const HeaderButton = () => {
+const NavButton = () => {
+  const navigate = useNavigate();
   return (
     <ButtonSection>
-      <p>버튼1</p>
-      <p>버튼2</p>
-      <p>버튼3</p>
+      <p>
+        <img className="manIcon" src={manIcon} onClick={navigate('/login')} />
+      </p>
+      <p>
+        <img src={message} onClick={navigate('/login')} />
+      </p>
+      <p>
+        <img className="burger" src={burger} onClick={'ds'} />
+      </p>
     </ButtonSection>
   );
 };
-const HeaderCategory = () => {
-  return <CategorySection></CategorySection>;
+const NavCategory = () => {
+  return (
+    <CategorySection>
+      <CategoryMain>
+        <p className="category_icon"></p>
+        <p className="category_text"></p>
+      </CategoryMain>
+      <CategoryMain>
+        <p className="category_icon"></p>
+        <p className="category_text"></p>
+      </CategoryMain>
+      <CategoryMain>
+        <p className="category_icon"></p>
+        <p className="category_text"></p>
+      </CategoryMain>
+      <CategoryMain>
+        <p className="category_icon"></p>
+        <p className="category_text"></p>
+      </CategoryMain>
+      <CategoryMain>
+        <p className="category_icon"></p>
+        <p className="category_text"></p>
+      </CategoryMain>
+      <CategoryMain>
+        <p className="category_icon"></p>
+        <p className="category_text"></p>
+      </CategoryMain>
+      <CategoryMain>
+        <p className="category_icon"></p>
+        <p className="category_text"></p>
+      </CategoryMain>
+    </CategorySection>
+  );
 };
 
 export default Header;
