58fa52c006e384ef65e635fb87f19fe43ac12120
JoEunsoo <dcpg95@gmail.com>
2022-09-29 16:42:57 +0900

    chore : 경로 수정

--- client/src/Components/ItemDetail/DetailMain.js
+++ client/src/Components/ItemDetail/DetailMain.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { saves, dateCompute } from '../../util/detailLogic';
+import { saves, dateCompute } from '../../util/RequestLogic.ts';
 import { Detail_Container, Detail_Section } from './DetailStyle.jsx';
 import ItemCategory from './ItemCategory';
 import DetailImg from './DetailImg';
