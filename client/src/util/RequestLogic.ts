32675425c210b91b1ad88de3f1b68165b7f47569
JoEunsoo <dcpg95@gmail.com>
2022-09-30 00:50:03 +0900

    chore : 코드수정

--- client/src/util/RequestLogic.ts
+++ client/src/util/RequestLogic.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
 
 type retutnType = (string | object)[];
 interface serverRes {
@@ -75,4 +78,5 @@ const latestItem = () => {
   return data;
 };
 
-export { saves, itemGet, dateCompute, latestItem };
+
+export { saves, itemGet, dateCompute, latestItem,};
