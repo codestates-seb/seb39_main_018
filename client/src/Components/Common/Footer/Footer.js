b26c105023bcfe4fca520c0d01a770dab6874504
JoEunsoo <dcpg95@gmail.com>
2022-09-30 19:49:24 +0900

    feat : footer 컴포넌트 구현

--- client/src/Components/Common/Footer/Footer.js
+++ client/src/Components/Common/Footer/Footer.js
@@ -1,45 +1,131 @@
 import React from 'react';
+import { BasicBtn } from '../Button/Button';
+import { InputUI } from '../Input/Input';
 import {
   FooterContainer,
-  FooterContent,
-  FooterTopBox,
-  FooterBottom,
-  FooterBottomBox,
+  MainSection,
+  IntroSection,
+  IntroTitle,
+  ConactSection,
+  LogoSection,
+  IntroNav,
+  SearchInput,
+  FooterIntro,
+  FooterConact,
+  ConactIcon,
+  FooterLogo,
+  ConactTop,
+  SubSection,
+  Copylight,
+  Notice,
 } from './Footerstyle';
 
-const Footer = () => {
-  return <FooterUI />;
-};
+import emailimg from '../../Imgs/email.png';
+import { InstaIcon, EmailIcon, GithubIcon, YoutubeIcon } from '../Icons/Icons';
 
-const FooterUI = () => {
+const Footer = () => {
   return (
     <FooterContainer>
-      <FooterTopBox>
-        <FooterContent>
-          <h3>INTRODUCE</h3>
-          <p>Home</p>
-          <p>Menu</p>
-          <p>About</p>
-          <p>Help</p>
-        </FooterContent>
-        <FooterContent>
-          <h3>CONACT</h3>
-          <p>Eunsoo</p>
-          <p>YeonJoo</p>
-          <p>HyeJin</p>
-          <p>JiHong</p>
-        </FooterContent>
-      </FooterTopBox>
-      <FooterBottomBox>
-        <FooterBottom>By Corporation Ⓒ2022</FooterBottom>
-        <FooterBottom>
-          <div className="service">
-            솔트 서비스 내 상품을 제외한 모든 상품은 개인간 거래로 이루어지며 , 구매 및 판매시
-            신중하게 진행하시길 바랍니다. 또한 거래에 대한 법적 책임을 지지 않습니다.
-          </div>
-        </FooterBottom>
-      </FooterBottomBox>
+      <FooterMain />
+      <FooterSub />
     </FooterContainer>
+  );
+};
+
+const FooterMain = () => {
+  return (
+    <MainSection>
+      <LeftSection />
+      <MiddleSection />
+      <RightSection />
+    </MainSection>
+  );
+};
+
+const LeftSection = () => {
+  return (
+    <IntroSection>
+      <FooterIntro>
+        <IntroTitle>
+          <p className="title">INTRODUCE</p>
+          <p className="border_line long" />
+        </IntroTitle>
+        <IntroNav>
+          <p className="top_intro">Home</p>
+          <p className="top_intro">Main</p>
+          <p className="top_intro">Menu</p>
+          <p className="top_intro">About</p>
+          <p className="top_intro end">Login</p>
+        </IntroNav>
+        <IntroNav>
+          <p className="bottom_intro">Eunsoo</p>
+          <p className="bottom_intro">YeonJoo</p>
+          <p className="bottom_intro">HyeJin</p>
+          <p className="bottom_intro end">JiHong</p>
+        </IntroNav>
+      </FooterIntro>
+    </IntroSection>
+  );
+};
+const MiddleSection = () => {
+  return (
+    <LogoSection>
+      <FooterLogo>
+        <p>SORT</p>
+      </FooterLogo>
+      <SearchInput>
+        <InputUI width="200px" height="15px" placeholder="이메일을 입력 해주세요" />
+        <BasicBtn width="75px" height="35px" background="lightpink" left="10px">
+          SEND
+        </BasicBtn>
+      </SearchInput>
+    </LogoSection>
+  );
+};
+
+const RightSection = () => {
+  return (
+    <ConactSection>
+      <FooterConact>
+        <ConactTop>
+          <p className="title">CONACT</p>
+          <p className="border_line" />
+        </ConactTop>
+        <ConactIcon>
+          <p className="github_icon">
+            <GithubIcon />
+          </p>
+        </ConactIcon>
+        <ConactIcon>
+          <p>
+            <YoutubeIcon />
+          </p>
+        </ConactIcon>
+        <ConactIcon>
+          <p>
+            <InstaIcon />
+          </p>
+        </ConactIcon>
+        <ConactIcon>
+          <p>
+            <EmailIcon />
+          </p>
+        </ConactIcon>
+      </FooterConact>
+    </ConactSection>
+  );
+};
+
+const FooterSub = () => {
+  return (
+    <SubSection>
+      <Copylight>By Copylight Ⓒ2022</Copylight>
+      <Notice>
+        {' '}
+        솔트 서비스 내 상품을 제외한 모든 상품은 개인간 거래로 이루어지며 , 구매 및 판매시 신중하게
+        진행하시길 바랍니다. 또한 거래에 대한 법적 책임을 지지 않습니다.
+      </Notice>
+    </SubSection>
   );
 };
 
--- client/src/Components/Common/Footer/Footerstyle.js
+++ client/src/Components/Common/Footer/Footerstyle.js
@@ -1,48 +1,159 @@
 import styled from 'styled-components';
 
 const FooterContainer = styled.div`
-  border-top: 1px solid var(--lightgray);
+  /* border-top: 1px solid var(--lightgray); */
   width: 100%;
-  height: 250px;
+  height: 300px;
+  background: #767676;
 `;
 
-const AllSetting = styled.div`
+const ParantSetting = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 500px;
+  height: 200px;
 `;
-
-const FooterTopBox = styled(AllSetting)`
-  border-bottom: 1px solid var(--lightgray);
-`;
-
-const FooterBottomBox = styled(AllSetting)`
+const ChilidSetting = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
-  padding: 10px;
+  justify-content: center;
+  width: 100%;
+  color: white;
 `;
 
-const FooterContent = styled(AllSetting)`
-  margin: 20px 150px;
-  flex-direction: column;
+const MainSection = styled(ParantSetting)`
+  width: 100%;
+  padding-top: 10px;
+  .title {
+    /* border: 1px solid black; */
+    width: 100%;
+    text-align: center;
+    font-size: 22px;
+    font-weight: bold;
+  }
+  .border_line {
+    margin-top: 5px;
+    width: 80px;
+    border: 1px solid;
+  }
+  .long {
+    width: 120px;
+  }
+`;
+const IntroSection = styled(ParantSetting)``;
+
+const FooterIntro = styled(ChilidSetting)`
+  height: 150px;
+  flex-wrap: wrap;
+  //border: 1px solid black;
+`;
+const IntroTitle = styled(ChilidSetting)`
+  flex-wrap: wrap;
+`;
+const IntroNav = styled(ChilidSetting)`
+  width: 100%;
+  font-size: 12px;
   p {
-    font-size: var(--f-small);
+    height: 11px;
+    padding: 0 12px;
+    border-right: 1px solid;
     cursor: pointer;
-    padding: 5px;
   }
-  h3 {
-    margin: 20px;
+  .top_intro {
+    padding: 0 20px;
   }
-`;
-
-const FooterBottom = styled.div`
-  font-size: var(--f-xsmall);
-  margin: 10px 10px;
-  .service {
-    font-size: var(--f-xsmall);
-    width: 455px;
+  .end {
+    border: none;
+  }
+  p:hover {
+    color: #c0c1c2;
   }
 `;
 
-export { FooterContainer, FooterTopBox, FooterContent, FooterBottomBox, FooterBottom };
+const ConactSection = styled(ParantSetting)``;
+const LogoSection = styled(ParantSetting)`
+  flex-wrap: wrap;
+  align-items: flex-start;
+  height: 130px;
+`;
+
+const SearchInput = styled(ChilidSetting)`
+  height: 120px;
+  //border: 1px solid black;
+`;
+const FooterLogo = styled(ChilidSetting)`
+  height: 50px;
+  //border: 1px solid black;
+  font-size: 50px;
+  align-items: flex-start;
+`;
+const FooterConact = styled(ChilidSetting)`
+  //border: 1px solid black;
+  height: 150px;
+  flex-wrap: wrap;
+  justify-content: center;
+  .icon {
+    width: 70px;
+    height: 70px;
+    background: lightcyan;
+    border-radius: 50%;
+  }
+`;
+
+const ConactIcon = styled(ChilidSetting)`
+  padding: 10px;
+  width: 20px;
+  height: 20px;
+  background: lightcyan;
+  border-radius: 50%;
+  font-size: 30px;
+  color: #393d42;
+  margin: 0 20px;
+  cursor: pointer;
+  p {
+    padding-top: 8px;
+  }
+  ${ConactIcon}:hover{
+    background:#8ca2b8;
+  }
+`;
+const ConactTop = styled(ChilidSetting)`
+  height: 30px;
+  flex-wrap: wrap;
+`;
+
+const SubSection = styled(ChilidSetting)`
+  margin-top: 30px;
+  border-top: 1px solid white;
+  height: 60px;
+`;
+const Notice = styled.div`
+  color: white;
+  width: 350px;
+  font-size: 10px;
+  margin-left: 30px;
+`;
+const Copylight = styled.div`
+  color: white;
+  font-size: 12px;
+`;
+
+export {
+  MainSection,
+  FooterContainer,
+  IntroSection,
+  IntroTitle,
+  IntroNav,
+  ConactSection,
+  LogoSection,
+  SearchInput,
+  FooterIntro,
+  FooterConact,
+  ConactIcon,
+  FooterLogo,
+  ConactTop,
+  SubSection,
+  Copylight,
+  Notice,
+};
