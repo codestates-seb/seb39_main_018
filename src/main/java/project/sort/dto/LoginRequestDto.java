9812397ba9a79790e8964218aa78400a376b9778
hyeding <65836744+hyeding@users.noreply.github.com>
2022-09-30 16:46:46 +0900

    Delete LoginRequestDto.java

--- src/main/java/project/sort/dto/LoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package project.sort.dto;
-
-import lombok.Data;
-@Data
-public class LoginRequestDto {
-    private String username;
-    private String password;
-    private String email; // 내가추가함
-}
