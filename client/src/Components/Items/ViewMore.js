0f36d743a336665b0e8bd60b4eb2ae1008481428
JoEunsoo <dcpg95@gmail.com>
2022-10-03 01:57:48 +0900

    refactor : 더보기 버튼 수정

--- client/src/Components/Items/ViewMore.js
+++ client/src/Components/Items/ViewMore.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { ViewMoreSection, ViewMoreBtn } from './MainStyle';
 import { BasicBtn } from '../Common/Button/Button';
 import { viewsCount } from '../../redux/itemslice';
-
+import { DownIcon } from '../Common/Icons/Icons';
 const ViewMore = ({ dispatch }) => {
   return (
     <ViewMoreSection>
       <ViewMoreBtn onClick={() => dispatch(viewsCount(8))}>
-        <BasicBtn background="white" color="balck">
-          더보기
+        <BasicBtn background="white" color="balck" size="25px">
+          <p>더보기</p>
+          <p>
+            <DownIcon />
+          </p>
         </BasicBtn>
       </ViewMoreBtn>
     </ViewMoreSection>
