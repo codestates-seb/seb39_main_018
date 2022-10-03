3e129e7f422a90e30bfd0d19bc1256b2861018e5
JoEunsoo <dcpg95@gmail.com>
2022-10-04 00:48:10 +0900

    chore : 이미지 업로드

---
+++ client/src/Components/Imgs/navbarimg/index.js
@@ -0,0 +1,7 @@
+import Sort from './SORT.svg';
+import message3 from './Vector-1.svg';
+import user from './Vector-2.svg';
+import message4 from './Vector.svg';
+import menu from '../menu.png';
+
+export default { Sort, message3, user, message4, menu };
