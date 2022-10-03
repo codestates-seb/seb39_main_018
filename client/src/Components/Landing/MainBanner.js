b465f88860eaca6c54b8ee64be69249c76500b68
yeonkr <1714882@sookmyung.ac.kr>
2022-10-04 01:04:59 +0900

    feat : 게시물 등록 페이지 구현

--- client/src/Components/Landing/MainBanner.js
+++ client/src/Components/Landing/MainBanner.js
@@ -8,7 +8,6 @@ import body2 from '../Imgs/body2.svg';
 import paper from '../Imgs/paper-airplane.png';
 import mainbanner from '../Imgs/Group 381.svg';
 
-
 const MainBanner = () => {
   return (
     <MainBanner_Container>
@@ -18,7 +17,7 @@ const MainBanner = () => {
       <img src={솔트} className="sort" />
       <img src={body1} className="body1" />
       <img src={body2} className="body2" /> */}
-      <img src={mainbanner} className='mainbanner'/>
+      <img src={mainbanner} className="mainbanner" />
     </MainBanner_Container>
   );
 };
