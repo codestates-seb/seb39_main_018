4087505ce17890ebcd273c369c60410bb9d10f74
JoEunsoo <dcpg95@gmail.com>
2022-10-03 22:15:09 +0900

    feat : 툴킷 기능 추가

--- client/src/redux/itemslice.ts
+++ client/src/redux/itemslice.ts
@@ -2,19 +2,26 @@ import { createSlice } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
+
 type InitialState = {
   value: number;
   isLoad: boolean;
   searchData: string;
   categorys: string[];
+  category: string;
+  tags: string[];
 };
+
 
 const initialState: InitialState = {
   value: 8,
   isLoad: false,
   searchData: '',
-  categorys: ['전체','연극/영화','숙박','전시','여행','스포츠','공연'],
+  categorys: ['전체', '연극/영화', '숙박', '전시', '여행', '스포츠', '공연'],
+  category: '',
+  tags: [],
 };
+
 const itemSlice = createSlice({
   name: 'item',
   initialState,
@@ -30,10 +37,21 @@ const itemSlice = createSlice({
       state.isLoad = action.payload;
     },
     searchitem: (state, action) => {
-      state.searchData = action.payload
+      state.searchData = action.payload;
     },
+    selectCategory: (state, action) => {
+      state.category = action.payload;
+    },
+    createTag: (state, action) => {
+      state.tags = [...state.tags, action.payload];
+    },
+    deleteTag: (state,action) => {
+      state.tags = state.tags.filter( taglist => taglist !== action.payload);
+    },
+    
   },
 });
 
 export default itemSlice;
-export const { viewsCount, resueRouter, closeCategory,searchitem } = itemSlice.actions;
+export const { viewsCount, resueRouter, closeCategory, searchitem, selectCategory, createTag , deleteTag } =
+  itemSlice.actions;
