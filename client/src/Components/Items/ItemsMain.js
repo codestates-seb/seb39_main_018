0c6111682990a893dc1a13fbcae66b05313c7025
JoEunsoo <dcpg95@gmail.com>
2022-10-04 20:07:09 +0900

    feat : 상품판매 버튼 추가

--- client/src/Components/Items/ItemsMain.js
+++ client/src/Components/Items/ItemsMain.js
@@ -6,7 +6,8 @@ import ViewMore from './ViewMore';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getItems } from '../../util/requestItem';
-
+import Sellbtn from '../Common/Button/Sellbtn';
+import { sellButton } from '../Imgs/mainImgs/imgExport';
 
 const ItemsMain = () => {
   const navigate = useNavigate();
@@ -16,15 +17,16 @@ const ItemsMain = () => {
   };
   const dispatch = useDispatch();
   const viewCount = useSelector((state) => state.items.value);
-  const keyowrd = useSelector(state => state.items.tags);
+  const keyowrd = useSelector((state) => state.items.tags);
   const dataloads = getItems();
   const itemsData = dataloads[0].slice(0, viewCount);
-  const isLoading = dataloads[1]
+  const isLoading = dataloads[1];
   // const baboo = getItems(`full?title=15장&body=연석`);
-  console.log(itemsData)
+  console.log(itemsData);
 
   return (
     <MainContainer>
+      <Sellbtn />
       <Filter />
       <MainContent>
         <ItemList itemsData={itemsData} pageMove={pageMove} />
