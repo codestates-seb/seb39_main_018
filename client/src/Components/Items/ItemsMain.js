43ca2e7a32db1b1808e367ae7025b869a8d4b003
JoEunsoo <dcpg95@gmail.com>
2022-10-05 15:46:11 +0900

    feat : 정렬,필터별 데이터 렌더링 로직 구현

--- client/src/Components/Items/ItemsMain.js
+++ client/src/Components/Items/ItemsMain.js
@@ -5,23 +5,27 @@ import Filter from './ItemFilter';
 import ViewMore from './ViewMore';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getItems } from '../../util/requestItem';
+import { getDetail, getItems } from '../../util/requestItem';
 import Sellbtn from '../Common/Button/Sellbtn';
 import { sellButton } from '../Imgs/mainImgs/imgExport';
 const ItemsMain = () => {
   const navigate = useNavigate();
+  // useEffect( ( ) => {
+  //   getItems(api+status+keyword);
+  // },[])
   const pageMove = (url) => {
     navigate(url);
     window.scrollTo(0, 0);
   };
+
   const dispatch = useDispatch();
   const viewCount = useSelector((state) => state.items.value);
-  const keyowrd = useSelector(state => state.items.tags);
-  const dataloads = getItems();
+  const { sort, status, keyword } = useSelector((state) => state.items.apiInfo);
+  const api = sort + status + keyword;
+  const dataloads = getItems(sort + status + keyword);
   const itemsData = dataloads[0].slice(0, viewCount);
-  const isLoading = dataloads[1]
-  // const baboo = getItems(`full?title=15장&body=연석`);
-  console.log(itemsData)
+  const isLoading = dataloads[1];
+  console.log(api);
 
   return (
     <MainContainer>
