2663f3b6c0d10f776ec486a22267e10d0cbf2aaf
JoEunsoo <dcpg95@gmail.com>
2022-10-05 15:47:33 +0900

    chore : 상품 상태관리 타입 추가

--- client/src/util/itemType.ts
+++ client/src/util/itemType.ts
@@ -1,13 +1,19 @@
 interface ItemType {
-    created_date: string;
-    id: number;
-    title: string;
-    body: string;
-    price: string;
-    end_date: string;
-    seat_number: string;
-    region: string;
-  }
+  created_date: string;
+  id: number;
+  title: string;
+  body: string;
+  price: string;
+  end_date: string;
+  seat_number: string;
+  region: string;
+}
 
+interface ApiType {
+  sort: string;
+  type: string;
+  status: string;
+  keyword: string;
+}
 
-  export {ItemType};
+export { ItemType, ApiType };
