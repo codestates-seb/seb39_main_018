966053c31f85901264ba8aefbf3a3d45c4e65e2b
JoEunsoo <dcpg95@gmail.com>
2022-10-11 18:54:31 +0900

    chore : 웹팩 파비콘 플러그인 설치

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconWebpackPlugin = require('favicons-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
@@ -71,6 +72,8 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      favicon: './public/favicon.ico',
     }),
+    new FaviconWebpackPlugin('./public/favicon.ico'),
   ],
 };
