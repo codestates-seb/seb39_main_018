0dabc5a86456ed8c4c2eea9cdd2db0f2daec77c1
JoEunsoo <dcpg95@gmail.com>
2022-10-11 19:53:21 +0900

    chore : 파일삭제

--- server/hyeding/src/main/java/com/main/posts/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.main.posts.repository;
-
-import com.main.posts.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Posts,Long> {
-}
