9479b8797b1ea24232dd17e9a96418e75e55fdfc
JoEunsoo <dcpg95@gmail.com>
2022-10-13 20:37:36 +0900

    chore : 변화없음

--- client/src/Components/Items/ViewMore/ViewMoreStyle.js
+++ client/src/Components/Items/ViewMore/ViewMoreStyle.js
@@ -1,5 +1,28 @@
 import styled from 'styled-components';
 
+const AllOption = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 10px 0;
+  width: 100%;
+
+  @media screen and (max-width: 720px) {
+    p {
+      font-size: 15px;
+    }
+  }
+  @media screen and (max-width: 620px) {
+    p {
+      font-size: 12px;
+    }
+  }
+  @media screen and (max-width: 450px) {
+    p {
+      font-size: 8px;
+    }
+  }
+`;
+
 const ViewMoreSection = styled(AllOption)`
   height: 50px;
   justify-content: center;
